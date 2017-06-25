30b163dbdd2d25520701b717ef03e1f74c897fec
DESKTOP-T2GM0H5\woong <lurend479>
2017-06-25 18:36:11 +0900

    던파 자유게시판을 크롤링 할려고 했지만 실패

--- routes/main_search.js
+++ routes/main_search.js
@@ -1,4 +1,6 @@
 var fs = require("fs");
+var cheerio = require("cheerio");
+var request = require("request");  
 
 exports.main_search = function(req,res){
 
@@ -9,21 +11,35 @@ exports.main_search = function(req,res){
 
 exports.main_search_result = function(req,res){
 
-    var keywordw = req.body.search;
-    console.log(keywordw);
+    var keyword = req.body.search;
+    console.log(keyword);
 
+    var url = "http://www.inven.co.kr/board/powerbbs.php?name=subject&keyword=" + keyword + "&x=22&y=2&come_idx=3911&iskin=&mskin=&p=1&query=list&my=&category=&sort=PID&orderby=&sterm=";
+    request(url, function(error, response, body){
+                var loadBody = cheerio.load(body);
+
+                var newsTitles = [];
+                var newsTitleHrefs = [];
+
+                var parsingFunction = function(index, data) {
+                var postTitle = loadBody(data).text();
+                var postHref = loadBody(data).attr("href");
+
+                newsTitles.push(postTitle);
+                newsTitleHrefs.push(postHref);
+                }
+                console.log(body);
+                var postElements = loadBody("table .sj_ln");
+                postElements.each(parsingFunction);
+
+                console.log(newsTitles);
+
+            })
+
+
+
+
+
+// 
     res.render('main_search_result'); 
-
-   //res.send({ keyword: keywordw });
-    //res.redirect("/result");
-
-
-    
-    // gRespons = res;
-    // gParsingData = null;
-    // var parsing = function (){
-    //     newsParsing.NewsDataParsing(ViewNewsData);  
-    // };
-
-    // parsing();
 }

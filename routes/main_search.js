feea45d1a51a134f957f1cc960bb82631ff83385
DESKTOP-T2GM0H5\woong <lurend479>
2017-06-25 12:54:08 +0900

    첫 세팅 시작!!

---
+++ routes/main_search.js
@@ -0,0 +1,11 @@
+exports.main = function(req,res){
+    
+    res.render('main_search');  
+    // gRespons = res;
+    // gParsingData = null;
+    // var parsing = function (){
+    //     newsParsing.NewsDataParsing(ViewNewsData);  
+    // };
+
+    // parsing();
+}

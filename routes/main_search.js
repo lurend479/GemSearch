58790695359a8eae4d22e32861a552af16d31239
DESKTOP-T2GM0H5\woong <lurend479>
2017-06-29 01:47:03 +0900

    최소 기능 추가

--- routes/main_search.js
+++ routes/main_search.js
@@ -1,6 +1,7 @@
 var fs = require("fs");
 var cheerio = require("cheerio");
 var request = require("request");  
+var urlencode = require('urlencode');
 
 exports.main_search = function(req,res){
 
@@ -9,37 +10,64 @@ exports.main_search = function(req,res){
     });
 }
 
+var isEmpty = function(value){ if( value == "" || value == null || value == undefined || ( value != null && typeof value == "object" && !Object.keys(value).length ) ){ return true }else{ return false } };
+var searchTitles = [];
+var searchTitleHrefs = [];
+var count = 0;
+var fullCount = 1000;
 exports.main_search_result = function(req,res){
 
     var keyword = req.body.search;
     console.log(keyword);
 
-    var url = "http://www.inven.co.kr/board/powerbbs.php?name=subject&keyword=" + keyword + "&x=22&y=2&come_idx=3911&iskin=&mskin=&p=1&query=list&my=&category=&sort=PID&orderby=&sterm=";
-    request(url, function(error, response, body){
-                var loadBody = cheerio.load(body);
+    searchTitles = [];
+    searchTitleHrefs = [];
+    count = 0;
+    var requestFunc = function (index, url){
+            var fullUrl = url + index;
+            request(fullUrl, function(error, response, body){
+                console.log(index);
+                
+                count++;
+                console.log(count);
+                if(isEmpty(body) == false)
+                {
+                    var loadBody = cheerio.load(body);
 
-                var newsTitles = [];
-                var newsTitleHrefs = [];
+                    var table = loadBody(".board_list1");
+                    
 
-                var parsingFunction = function(index, data) {
-                var postTitle = loadBody(data).text();
-                var postHref = loadBody(data).attr("href");
+                    var parsingFunction = function(index, data) {
+                        if(loadBody(data).hasClass(".ls .tr") == true)
+                        {
+                        
+                        }
+                        var postTitle = loadBody(data).find(".sj_ln").text();
+                            var postHref = loadBody(data).find(".sj_ln").attr("href");
 
-                newsTitles.push(postTitle);
-                newsTitleHrefs.push(postHref);
+                            searchTitles.push(postTitle);
+                            searchTitleHrefs.push(postHref);
+                    }
+
+                    var postElements = loadBody("table tr .tr");
+                    postElements.each(parsingFunction);
                 }
-                //console.log(body);
-                var postElements = loadBody("table .sj_ln");
-                postElements.each(parsingFunction);
-
-                console.log(newsTitles);
-
+                
+                if(count >= fullCount)
+                {
+                    count = 0;
+                    res.render('main_search_result', {searchTitles: searchTitles, searchTitleHrefs: searchTitleHrefs});
+                }
             })
+        };
+
+    var url = "http://www.inven.co.kr/board/powerbbs.php?name=subject&keyword="+urlencode(keyword)+"&come_idx=";
+
+    for(var index = 3000 ; index < 3000 + fullCount ; ++index)
+    {
+        requestFunc(index, url);
+    }
 
 
-
-
-
-// 
-    res.render('main_search_result'); 
+     
 }

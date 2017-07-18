f816d0963be2994174531050b5763a3565080e97
DESKTOP-T2GM0H5\woong <lurend479>
2017-07-19 00:56:09 +0900

    기본기능 추가중

---
+++ public/js/SearchFunc.js
@@ -0,0 +1,35 @@
+var cheerio = require("cheerio");
+
+
+
+var Search_Inven = function(loadbody)
+{
+    var list = [];
+
+    list.push(new GameSearchResultData("네이버", "www.naver.com"));
+    
+    
+    
+    
+    
+    
+    
+    
+    // var table = loadBody(".board_list1");
+                    
+
+    // var parsingFunction = function(index, data) {
+    //     if(loadBody(data).hasClass(".ls .tr") == true)
+    //     {
+        
+    //     }
+    //     var postTitle = loadBody(data).find(".sj_ln").text();
+    //         var postHref = loadBody(data).find(".sj_ln").attr("href");
+
+    //         searchTitles.push(postTitle);
+    //         searchTitleHrefs.push(postHref);
+    // }
+
+    // var postElements = loadBody("table tr .tr");
+    // postElements.each(parsingFunction);
+}
